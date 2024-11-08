990bb41e241130c20a9245bfee1de9cb1722ac48
Reyhan SS <140525729+ReyyHan-21@users.noreply.github.com>
2024-11-08 09:56:58 +0700

    Latest

---
+++ lib/drawer.dart
@@ -0,0 +1,140 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          '',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.blue[900],
+        foregroundColor: Colors.white, //for universal colors
+      ),
+      drawer: buildGroupDrawer(context),
+      body: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.5,
+          mainAxisSpacing: 10.0,
+          shrinkWrap: true,
+          children: List.generate(10, (index) {
+            return Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  height: 5.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Colors.blue[200],
+                  ),
+                  child: Text(
+                    'Materi $index',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ));
+          })),
+    );
+  }
+
+  Drawer buildGroupDrawer(context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue[900],
+            ),
+            child: const Text(
+              'Alfando Reyhan Saputra',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30.0,
+              ),
+            ),
+          ),
+          ExpansionTile(
+            title: const Text('Materi'),
+            children: <Widget>[
+              ListTile(
+                title: Text('Materi 1'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Materi 2'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Materi 3'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Materi 4'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Materi 5'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+          ExpansionTile(
+            title: const Text('Tugas'),
+            children: <Widget>[
+              ListTile(
+                title: Text('Tugas 1'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Tugas 2'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Tugas 3'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Tugas 4'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Tugas 5'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:drawer_listview/drawer.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static const String header = 'XII RPL 2';
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: header),
+    );
+  }
+}
